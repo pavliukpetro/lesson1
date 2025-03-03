d98377fe434b8e20e48c5f17c5b34738d3021f9a
Peter Pavliuk <hartryfock@gmail.com>
2025-03-03 18:57:58 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// 1. find a button;
+// 2. add click event handler;
+// 3. create a function to send a message;
+// 4. call a function to send a message when the button is clicked;
+
+alert('1');
+[(1, 2)].forEach(alert);
